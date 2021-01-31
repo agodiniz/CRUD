0ad01d50a310fd16c0516216c9bccc105933b16b
Tiago Diniz <tmmidiatiago@gmail.com>
2021-01-31 11:37:35 -0300

    instalação composer e autoload

---
+++ public/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '610de1ecc2ebbcf1a62ec88897ccb62b66c37a97',
+    'name' => 'tarefas/ic',
+  ),
+  'versions' => 
+  array (
+    'tarefas/ic' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '610de1ecc2ebbcf1a62ec88897ccb62b66c37a97',
+    ),
+  ),
+);
