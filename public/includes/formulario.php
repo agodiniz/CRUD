037d7dbccfd5bbc643bb072c4e1ad20a49026aa6
Tiago Diniz <tmmidiatiago@gmail.com>
2021-02-06 10:00:51 -0300

    cadastrar e formulario

---
+++ public/includes/formulario.php
@@ -0,0 +1,7 @@
+<main>
+    <section>
+        <a href="index.php">
+            <button class="btn btn-outline-secondary"><i class="bi bi-arrow-left"></i>Voltar</button>
+        </a>
+    </section>
+</main>
